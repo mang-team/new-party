0c5986b63de966a2dc4f7cd18c0f6da6b4b6aa31
FeiWuSama <1825578184@qq.com>
2025-09-10 15:08:54 +0800

    feat-用户模块接口已完成

---
+++ src/main/java/com/itmang/aop/GlobalAspect.java
@@ -0,0 +1,51 @@
+package com.itmang.aop;
+
+import com.itmang.annotation.GlobalInterceptor;
+import com.itmang.constant.MessageConstant;
+import com.itmang.exception.BaseException;
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.lang.reflect.Method;
+
+@Aspect
+@Component
+@Slf4j
+public class GlobalAspect {
+
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    @Before("@annotation(com.itmang.annotation.GlobalInterceptor)")
+    public void interceptor(JoinPoint joinPoint){
+        Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
+        GlobalInterceptor interceptor = method.getAnnotation(GlobalInterceptor.class);
+        if(interceptor == null){
+            return;
+        }
+        if(interceptor.checkLogin()){
+            checkLogin();
+        }
+    }
+
+    private void checkLogin(){
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        String token = request.getHeader("token");
+        if(StringUtils.isEmpty(token)){
+            throw new BaseException(MessageConstant.USER_NOT_LOGIN);
+        }
+        if(!redisTemplate.hasKey("blacklist:" + token)){
+            throw new BaseException(MessageConstant.USER_NOT_LOGIN);
+        }
+    }
+}
