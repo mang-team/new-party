f90651537f4ec1c9565dd5a17d893ec758689d98
FeiWuSama <1825578184@qq.com>
2025-11-02 18:16:35 +0800

    feat-向请求拦截器配置增加对管理接口的拦截

--- src/main/java/com/itmang/config/WebMvcConfiguration.java
+++ src/main/java/com/itmang/config/WebMvcConfiguration.java
@@ -35,6 +35,7 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
         //log.info("开始注册自定义拦截器...");
         registry.addInterceptor(jwtTokenAdminInterceptor)
                 .addPathPatterns("/user/**")
+                .addPathPatterns("/admin/**")
                 .addPathPatterns("/action/**")
                 .addPathPatterns("/study/**")
                 .addPathPatterns("/party/**")
